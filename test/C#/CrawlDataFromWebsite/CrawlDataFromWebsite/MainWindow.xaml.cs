e182509909b4de758310967f7fed6dd6479541f4
laquocbuu309 <61673754+laquocbuu309@users.noreply.github.com>
2020-03-02 17:25:39 +0700

    Update

--- test/C#/CrawlDataFromWebsite/CrawlDataFromWebsite/MainWindow.xaml.cs
+++ test/C#/CrawlDataFromWebsite/CrawlDataFromWebsite/MainWindow.xaml.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,42 +24,72 @@ namespace CrawlDataFromWebsite
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Propertises
         ObservableCollection<MenuTreeItem> TreeItems;
+
+        HttpClient httpClient;
+        HttpClientHandler handler;
+        CookieContainer cookie = new CookieContainer();
+
+        #endregion
         public MainWindow()
         {
             InitializeComponent();
+            InitHttpClient();
 
             TreeItems = new ObservableCollection<MenuTreeItem>();
-            MenuTreeItem item1 = new MenuTreeItem() { Name = "Item1", URL = "vvv",
-                Items = new ObservableCollection<MenuTreeItem>()
-                {
-                    new MenuTreeItem(){Name = "Sub item 1", URL = "vvv"},
-                    new MenuTreeItem(){Name = "Sub item 1", URL = "vvv"},
-                    new MenuTreeItem(){Name = "Sub item 1", URL = "vvv"}
-                }
-            };
-
-            MenuTreeItem item2 = new MenuTreeItem()
-            {
-                Name = "Item2",
-                URL = "vvv",
-                Items = new ObservableCollection<MenuTreeItem>()
-                {
-                    new MenuTreeItem(){Name = "Sub item 2", URL = "vvv"},
-                    new MenuTreeItem(){Name = "Sub item 2", URL = "vvv"},
-                    new MenuTreeItem(){Name = "Sub item 2", URL = "vvv"}
-                }
-            };
-            AddItemIntoTreeViewItem(TreeItems, item1);
-            AddItemIntoTreeViewItem(TreeItems, item2);
             treeMain.ItemsSource = TreeItems;
 
         }
 
         #region Method
+        void InitHttpClient()
+        {
+            handler = new HttpClientHandler()
+            {
+                CookieContainer = cookie,
+                ClientCertificateOptions = ClientCertificateOption.Automatic,
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
+                AllowAutoRedirect = true,
+                UseDefaultCredentials = false
+            };
+
+            httpClient = new HttpClient(handler);
+            //httpClient.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36");
+             /*
+              * Header :
+              * - origin
+              * - Host
+              * - referer
+              * - :scheme
+              * - Accept
+              * - Accept-Encoding
+              * - Accept- Language
+              * - Users - Agent
+              */
+            
+            httpClient.BaseAddress = new Uri("https://www.howkteam.vn/");
+        }
+
         void AddItemIntoTreeViewItem(ObservableCollection<MenuTreeItem> root, MenuTreeItem node)
         {
             root.Add(node);
+        }
+
+        string CrawlDataFromURL(string url)
+        {
+            string html = "";
+            // Get
+            html = httpClient.GetStringAsync(url).Result;
+            // Post
+            //html = httpClient.PostAsync(url,new StringContent("")).Result.Content.ReadAsStringAsync().Result; 
+            return html;
+        }
+
+        void Crawl(string url)
+        {
+            string htmlLearn = CrawlDataFromURL(url);
+            //var courseList = Regex.Matches(htmlLearn, "")
         }
         #endregion
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -73,6 +106,11 @@ namespace CrawlDataFromWebsite
                 }
             };
             AddItemIntoTreeViewItem(TreeItems, item3);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            wbMain.Text = CrawlDataFromURL("learn");
         }
     }
 }
